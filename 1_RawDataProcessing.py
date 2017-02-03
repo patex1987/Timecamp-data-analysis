a1da75f1853de0a43def0c7c2c1167145fdb45fc
Patex <Patex@192.168.1.37>
2017-02-03 08:03:29 +0100

    RawDataOrocessing.py renamed

--- 1_RawDataProcessing.py
+++ 1_RawDataProcessing.py
@@ -1,20 +1,10 @@
 import re
-import os, errno
-
-
-# SilentRemove - removes the file, if the file doesnt exist then skips the exception
-# Useful at the initialisation of the program, to delete the previosuly created files
-def SilentRemove(filename):
-    try:
-        os.remove(filename)
-    except OSError as e: # this would be "except OSError, e:" before Python 2.6
-        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
-            raise # re-raise exception if a different error occured
+import HelpFunctions
 
 # Divides the raw input file into 
 def DivideIntoTimeAndData(fileName, origCharSeparator,newCharSeparator, origLevelSeparator, newLevelSeparator):
-    SilentRemove("2_Times.txt")
-    SilentRemove("3_Tasks.txt")
+    HelpFunctions.SilentRemove("2_Times.txt")
+    HelpFunctions.SilentRemove("3_Tasks.txt")
     with open(fileName, 'r') as fHandle:
         for line in fHandle:
             line.rstrip()
